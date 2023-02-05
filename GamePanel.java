682fc646ccc8ae05420b0225deb8269192968267
AAhsanSN <ahsanahmer0@gmail.com>
2023-02-04 22:32:43 -0500

    work on with gamepanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,3 +1,28 @@
-public class GamePanel {
-    
+import javax.swing.JPanel;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class GamePanel extends JPanel implements AncestorListener{
+
+    GamePanel()[
+        
+    ]
 }
+
+    @Override
+    public void ancestorAdded(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorRemoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void ancestorMoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
