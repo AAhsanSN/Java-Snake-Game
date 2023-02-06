723c410cbfed0b8a066be386ef638cc53c7860df
AAhsanSN <ahsanahmer0@gmail.com>
2023-02-05 20:59:51 -0500

    got more problem in GamePanel

--- GamePanel.java
+++ GamePanel.java
@@ -1,13 +1,34 @@
+import javax.crypto.KeyAgreement;
 import javax.swing.JPanel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
-public class GamePanel extends JPanel implements AncestorListener{
+public class  GamePanel<Graphics> extends JPanel implements AncestorListener{
 
     GamePanel(){
 
     }
+    public void startGame() {
 
+    }
+    public void paintComponent(Graphics g) {
+
+    }
+    public void draw(Graphics g) { 
+
+    }
+    public void move() {
+
+    }
+    public void checkApple() {
+
+    }
+    public void checkCollisions() {
+        
+    }
+    public void gameOver(Graphics g) {
+
+    }
     @Override
     public void ancestorAdded(AncestorEvent event) {
         // TODO Auto-generated method stub
@@ -25,5 +46,16 @@ public class GamePanel extends JPanel implements AncestorListener{
         // TODO Auto-generated method stub
         
     }
-}
 
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+
+    }
+
+
+    
+    
+}
