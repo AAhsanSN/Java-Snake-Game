fdaae5dcf82386a7a3f40065f4b1bbd990872079
AAhsanSN <ahsanahmer0@gmail.com>
2023-02-06 21:53:00 -0500

    fix problem in game panel with AncestorEvent event

--- GamePanel.java
+++ GamePanel.java
@@ -1,6 +1,7 @@
 import java.awt.*;
-import javax.crypto.KeyAgreement;
+import java.awt.event.*;
 import javax.swing.JPanel;
+import javax.crypto.KeyAgreement;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
@@ -53,6 +54,22 @@ public class  GamePanel extends JPanel implements AncestorListener{
        
         public void keyPressed(KeyAgreement e){}
 
+    }
+
+    @Override
+    public void ancestorAdded(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void ancestorRemoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
+    }
+    @Override
+    public void ancestorMoved(AncestorEvent event) {
+        // TODO Auto-generated method stub
+        
     }
 
 
